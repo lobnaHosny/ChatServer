7e422bf7bf27a10c6222c439126591b2e53e97e3
Tariq1997 <tariq_yazid@hotmail.com>
2019-12-20 11:42:12 +0800

    Client is class
    Created Client Class
    With 2 methods for reading and sending

--- ChatServer/ChatServer/src/Client/Client.java
+++ ChatServer/ChatServer/src/Client/Client.java
@@ -9,25 +9,37 @@ public class Client
     private static DataOutputStream dos;
     private static DataInputStream dis;
     final static int ServerPort = 8080;
+    Socket s = null;
 
-    public static void main(String args[])
+    public Client(int ServerPort)
     {
         Scanner scn = new Scanner(System.in);
 
         // getting localhost ip
         try {
-            InetAddress ip = InetAddress.getByName("localhost");
+            InetAddress ip = InetAddress.getByName("127.0.0.1");
 
 
-        // establish the connection
-        Socket s = new Socket("127.0.0.1", ServerPort);
+            // establish the connection
+            s = new Socket(ip, ServerPort);
+
+            // obtaining input and out streams
+            dis = new DataInputStream(s.getInputStream());
+            dos = new DataOutputStream(s.getOutputStream());
+            if(dis != null){
+                readMessage();
+                sendMessage(scn);
+            }
+
+        } catch (Exception e) {
+            System.err.println("wrong port");
+            //e.printStackTrace();
+        }
 
 
-        // obtaining input and out streams
-        dis = new DataInputStream(s.getInputStream());
-        dos = new DataOutputStream(s.getOutputStream());
+    }
 
-
+    public void sendMessage(Scanner scn){
         // sendMessage thread
         Thread sendMessage = new Thread(new Runnable()
         {
@@ -37,7 +49,6 @@ public class Client
 
                     // read the message to deliver.
                     String msg = scn.nextLine();
-
                     try {
                         // write on the output stream
                         dos.writeUTF(msg);
@@ -47,7 +58,9 @@ public class Client
                 }
             }
         });
-
+        sendMessage.start();
+    }
+    public void readMessage(){
         Thread readMessage = new Thread(new Runnable()
         {
             @Override
@@ -56,7 +69,7 @@ public class Client
                 while (true) {
                     try {
                         // read the message sent to this client
-                        if(dis.available()>0){
+                        if(dis.available()>0 && dis!=null){
                             String msg = dis.readUTF();
 
                             if(!msg.equals("Update List")){
@@ -65,27 +78,27 @@ public class Client
                             if(msg.equals("QUIT")){
                                 s.close();
                                 dis.close();
+                                dos.close();
                                 System.exit(0);
-                                break;
+                                //break;
                             }
                         }
 
                     } catch (IOException e) {
 
+                        //System.err.println(e);
                         e.printStackTrace();
                     }
                 }
             }
         });
-       readMessage.start();
-       sendMessage.start();
+        readMessage.start();
+    }
 
 
-        } catch (Exception e) {
-            System.err.println("wrong port");
-            //e.printStackTrace();
-        }
-
-
+    public static void main(String args[])
+    {
+        Client client = new Client(ServerPort);
     }
 }
+
